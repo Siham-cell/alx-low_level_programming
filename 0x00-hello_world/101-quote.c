76e2fed58181c77f7c7bce58bde78e3ea7e2584e
Siham-cell <Sihuabdi@gmail.com>
2022-03-14 03:53:33 -0700

    UNIX is basicaly a unique operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
 /**
  * main - entry point
  *
- * Return: return 1 after program execution
+ *Return: Always 1 (Success)
  */
 int main(void)
-printf("and that piece of art is useful");
-return (0);
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
